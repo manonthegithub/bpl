651e448b352df9f2519549317fc2f73493448a6a
Kirill Yankov <myworkpostbox@gmail.com>
2016-12-05 00:04:41 +0300

    Move to heroku

--- embedded/src/main/java/ru/bookpleasure/launch/Main.java
+++ embedded/src/main/java/ru/bookpleasure/launch/Main.java
@@ -13,6 +13,11 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        String rootPath = new File(".").getCanonicalPath();
+
+        System.out.println(rootPath);
+
+
         String catalinaHome = "..";
 
         Tomcat tomcat = new Tomcat();
